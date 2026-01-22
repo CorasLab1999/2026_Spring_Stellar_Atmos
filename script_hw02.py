a3387c102ee06c86b66f072765639585e78528c6
CorasLab1999 <coraanndefran@gmail.com>
2026-01-22 16:54:25 -0500

    start hw2

---
+++ script_hw02.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 22 16:51:18 2026
+
+@author: cad6543
+"""
+
+# Imports
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import style
+from astropy import units as u
+from astropy import constants as const
+
+import StellAst
